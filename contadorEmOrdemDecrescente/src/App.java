69e46022f5993f1043c6fa974c8c0397f643cb4a
barrosjunior26 <barrosjunior26@outlook.com.br>
2022-08-21 13:22:19 -0300

    terceiro commit - restruturação do bloco de códigos. Foi adicionado um loop para que o usuário possa realizar uma nova operação ou paneas sair dela escolhendo uma das opções.

--- contadorEmOrdemDecrescente/src/App.java
+++ contadorEmOrdemDecrescente/src/App.java
@@ -5,23 +5,32 @@ public class App {
         //
         //Entrar com dois números e imprimi-los em ordem decrescente (suponha números diferentes).
         //
-        //Solicitando a entrada de dados ao usuário
-        System.out.print("Digite o primeiro número: ");
-        int num1 = leia.nextInt();
-        System.out.print("Digite o segundo número: ");
-        int num2 = leia.nextInt();
+        //Declaração de variáveis
+        int opcao = 0;
         //
-        //Processamento dos dados e exibição do resultado para o usuário
-        if (num1 > num2) {
-            for(num1 = num1; num1 >= num2; num1--){
-                System.out.println(num1);
+        //Adicionando um loop para que o usuário possa repetir a operação ou apenas sair dela.
+        do{
+            //Solicitando a entrada de dados ao usuário
+            System.out.print("Digite o primeiro número: ");
+            int num1 = leia.nextInt();
+            System.out.print("Digite o segundo número: ");
+            int num2 = leia.nextInt();
+            //
+            //Processamento dos dados e exibição do resultado para o usuário
+            if (num1 > num2) {
+                for(num1 = num1; num1 >= num2; num1--){
+                    System.out.println(num1);
+                }
+            }else if (num2 > num1) {
+                for(num2 = num2; num2 >= num1; num2--){
+                    System.out.println(num2);
+                }
+            }else{
+                System.out.println("\nOS VALORES DIGITADOS SÃO IGUAIS, REPITA A OPERAÇÃO");
             }
-        }else if (num2 > num1) {
-            for(num2 = num2; num2 >= num1; num2--){
-                System.out.println(num2);
-            }
-        }else{
-            System.out.println("\nOS VALORES DIGITADOS SÃO IGUAIS, REPITA A OPERAÇÃO");
-        }
+            System.out.print("Digite 1 para nova operação ou 0 para sair do sistema: ");
+            opcao = leia.nextInt();
+        }while(opcao != 0);
+        System.out.println("Saindo do sistema...");
     }
 }
